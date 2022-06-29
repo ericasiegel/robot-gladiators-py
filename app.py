43b95e729a5ed4752954f120812928efb2eca855
Erica Siegel <siegel.erica@gmail.com>
2022-06-28 20:29:20 -0700

    created enemy and player classes with a fight function

--- app.py
+++ app.py
@@ -3,25 +3,46 @@ import random
 class Player:
     def __init__(self, player_name):
         self.name = player_name.upper()
-    health = 100
-    attack = 10
-    money = 10
+        self.health = 100
+        self.attack = 10
+        self.money = 10
 
 class Enemy:
     def __init__(self, enemy_name):
         self.name = enemy_name
         self.health = random.randint(19, 51)
         self.attack = random.randint(9, 21)
-        
+
 class Game:
-    def player_attack(self, enemy_health, player_attack):
-        return enemy_health - player_attack
-    def enemy_attack(self, player_health, enemy_attack):
-        return player_health - enemy_attack
+    # def fight(self, **args):
+    #     new_enemy_health = enemy_health - player_attack
+    #     new_player_health = player_health - enemy_attack
+        
+    # def player_attack(self, enemy_health, player_attack):
+    #     return enemy_health - player_attack
+    # def enemy_attack(self, player_health, enemy_attack):
+    #     return player_health - enemy_attack
     def skip(self, player_money):
         return player_money - 2
     def store(self):
         pass
+
+def fight(**kwargs):
+    # game = Game()
+    print(kwargs['enemy'])
+    print(kwargs['player'])
+    current_enemy = kwargs['enemy']
+    current_player = kwargs['player']
+    
+    # enemy attack
+    player_health = current_player['health'] - current_enemy['attack']
+    # print(player_health)
+    # player attack
+    enemy_health = current_enemy['health'] - current_player['attack']
+    # print(enemy_health)
+    
+    return player_health, enemy_health
+
 
 def play():
     print('Welcome to Robot Gladiators!')
@@ -38,18 +59,29 @@ def play():
     print(f"ENEMY : {enemy.name} = Health: {enemy.health}, Attack: {enemy.attack}")
     print('')
     
-    game = Game()
-    # first attack
-    player.health = game.enemy_attack(player.health, enemy.attack)
-    enemy.health = game.enemy_attack(enemy.health, player.attack)
-    print(f"{enemy.name} gets attacked by {player.name}")
-    print(f"{enemy.name} has {enemy.health} health remaining")
-    print('')
-    print(f"{player.name} gets attacked by {enemy.name}")
-    print(f"{player.name} has {player.health} health remaining")
-    
-    # prompt_fight = input("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'Skip' to choose. ")
-    # print('')
+    while player.health > 0 or enemy.health > 0:
+        prompt_fight = input("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'Skip' to choose. ")
+        print('')
+        if 'fight' in prompt_fight.lower():
+            print()
+            player.health, enemy.health = fight(enemy=vars(enemy), player=vars(player))
+            # print(player.health)
+            # print(enemy.health)
+            print(f"{enemy.name} gets attacked by {player.name}")
+            print(f"{enemy.name} has {enemy.health} health remaining")
+            print('')
+            print(f"{player.name} gets attacked by {enemy.name}")
+            print(f"{player.name} has {player.health} health remaining")
+            
+        elif 'skip' in prompt_fight.lower():
+            print('fight skipped')
+            
+        else:
+            print('fight ended')
+    else:
+        print('game ended')     
+            
+
 
 
 play()
